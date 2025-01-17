98216be39b4a42dd9889abf0b1055e915d3f884a
SKTVSM <Sachin.Kadlag@tvsmotor.com>
2025-01-17 19:33:01 +0530

    reading browser from xml in basetest and adding 2 xml -crossbrowser and master xml

--- src/test/java/TestBase/baseTest.java
+++ src/test/java/TestBase/baseTest.java
@@ -6,9 +6,12 @@ import org.apache.logging.log4j.Logger;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -19,12 +22,26 @@ public class baseTest {
     public Logger logger; //log variable
 
 
+    @Parameters({"os","browser"})
     @BeforeClass
-    public void setup(){
+    public void setup(String os,String br){
 
-        logger= LogManager.getLogger(this.getClass()); // log initiation for all method
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        logger= LogManager.getLogger(this.getClass());
+
+
+        //passing brower from xml file for excution
+        switch (br.toLowerCase()) {
+
+            case "chrome":driver= new ChromeDriver();break;
+            case "edge":driver= new EdgeDriver();break;
+            case "firefox":driver= new FirefoxDriver();break;
+            default:
+                System.out.println("Invalid Browser name");
+                return;
+        }
+        // log initiation for all method
+       // WebDriverManager.chromedriver().setup();
+       // driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
